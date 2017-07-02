3a64baa72ece08980fc10d584f24f85cf6434cfd
gubon-ug <bonukgu@postype.com>
2017-07-02 20:41:41 +0900

    list filter 사용

--- src/main/kotlin/com/rainist/ioextended/Main.kt
+++ src/main/kotlin/com/rainist/ioextended/Main.kt
@@ -2,5 +2,10 @@ package com.rainist.ioextended
 
 
 fun solve(combinedList: List<Long>): List<Long> {
-    return emptyList()
+    val a = combinedList.filter { it.rem(3) == 0.toLong() }.toMutableList()
+    a.distinctBy { it.rem(4) == 0.toLong() }.forEach { v ->
+        for (i in 1..a.filter { it == v.div(4).times(3) }.count())
+            a.remove(v)
+    }
+    return a
 }
