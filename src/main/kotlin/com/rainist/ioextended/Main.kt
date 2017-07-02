ac07e1f42340caffe3866912003893c7792b7741
gubon-ug <bonukgu@postype.com>
2017-07-02 21:50:11 +0900

    distinctBy를 filter로 교체

--- src/main/kotlin/com/rainist/ioextended/Main.kt
+++ src/main/kotlin/com/rainist/ioextended/Main.kt
@@ -3,7 +3,7 @@ package com.rainist.ioextended
 
 fun solve(combinedList: List<Long>): List<Long> {
     val a = combinedList.filter { it.rem(3) == 0.toLong() }.toMutableList()
-    a.distinctBy { it.rem(4) == 0.toLong() }.forEach { v ->
+    a.filter{it.rem(4) == 0.toLong()}.distinct().forEach{ v ->
         for (i in 1..a.filter { it == v.div(4).times(3) }.count())
             a.remove(v)
     }
